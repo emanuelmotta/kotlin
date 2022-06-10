1f5910256c70b0fed3ce52296f08add8e52ef99d
emanuelmotta <89169215+emanuelmotta@users.noreply.github.com>
2022-06-09 20:59:16 -0400

    kotlin

---
+++ soma.kt
@@ -0,0 +1,18 @@
+fun sum(x: Int, y: Int): Int {                                          
+    return x + y
+}
+
+fun subtracao(x: Int, y: Int): Int {                                          
+    return x - y
+}
+
+
+//fun multiply(x: Int, y: Int) = x * y                                    
+
+fun main() {
+    println("Soma de dois números")                                                              
+    println(sum(4, 3))   
+    println ("Subtração de dois números")
+    println(subtracao(4,0))                                               
+  //  println(multiply(2, 4))                                             
+}
---
+++ sub.kt
@@ -0,0 +1,23 @@
+fun sum(x: Int, y: Int): Int {                                          
+    return x + y
+}
+
+//fun multiply(x: Int, y: Int) = x * y                                    
+
+fun main() {
+    println("Soma de dois números")                                                              
+    println(sum(4, 3))                                                  
+  //  println(multiply(2, 4))                                             
+}
+
+fun Subtração(x: Int, y: Int): Int {                                          
+    return x - y
+}
+
+//fun multiply(x: Int, y: Int) = x * y                                    
+
+fun main() {
+    println("Subtração")                                                              
+    println(Subtração(4, 3))                                                  
+  //  println(multiply(2, 4))                                             
+}
